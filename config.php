7cddd4e78598536f108981a25ac1ca2e75466883
Pedro Tauzen <pedrotauzen@gmail.com>
2023-12-16 14:14:54 +0000

    config.php configured the database connection.

--- config.php
+++ config.php
@@ -1,2 +1,11 @@
 <?php
 
+// Define the database parameters.
+const DB_HOST = "localhost";
+const DB_USER = "root";
+const DB_PASS = "";
+const DB_NAME = "shareboard";
+
+// Define
+const ROOT_PATH = "/daw/TP3/";
+const ROOT_URL = "http://localhost/daw/TP3/";
