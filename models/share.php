e7a71aee1bddfa5ac55b98f745336bd41ffcd83f
Pedro Tauzen <pedrotauzen@gmail.com>
2023-12-16 16:01:09 +0000

    Models added

---
+++ models/share.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace models;
+
+use classes\Messages;
+
+class ShareModel extends Model
+{
+    public function Index()
+    {
+        $this->query('SELECT * FROM shares ORDER BY create_date DESC');
+        $rows = $this->resultSet();
+        return $rows;
+    }
+
+    public function add()
+    {
+        $post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+        if ($post['submit']) {
+            if ($post['title'] == '' || $post['body'] == '') {
+                Messages::setMsg('Please fill in all fields', 'error');
+                return;
+            }
+            $this->query('INSERT INTO shares (title, body, link, user_id) VALUES (:title, :body, :link, :user_id)');
+            $this->query(':title', $post['title']);
+            $this->query(':body', $post['body']);
+            $this->query(':link', $post['link']);
+            $this->query(':user_id', 1);
+            $this->execute();
+
+            if ($this->lastInsertId()) {
+                header('Location' . ROOT_URL . 'shares');
+            }
+        }
+        return;
+    }
+}
---
+++ models/user.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace models;
+
+use classes\Messages;
+
+class UserModel extends Model
+{
+    public function register(): void
+    {
+        $post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+        $password = md5($post['password']);
+
+        if ($post['name'] == '' || $post['email'] == '' || $post['password'] == '') {
+            Messages::setMsg('Please fill in all fields', 'error');
+            return;
+        }
+        $this->query('INSERT INTO users (name, email, password) VALUES (:name, :email, :password)');
+        $this->bind(':name', $post['name']);
+        $this->bind(':email', $post['email']);
+        $this->bind(':password', $password);
+        $this->execute();
+
+        if ($this->lastInsertId()) {
+            //Redirect
+            header('Location:' . ROOT_URL . 'users/login');
+        }
+        return;
+    }
+
+    public function login()
+    {
+        $post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+        $password = md5($post['password']);
+
+        if ($post['submit']) {
+            //Compare login
+            $this->query('SELECT * FROM users WHERE email = :email AND password = :password');
+            $this->bind(':email', $post['email']);
+            $this->bind(':password', $password);
+
+            $row = $this->single();
+
+            if ($row) {
+                $_SESSION['is_logged_in'] = true;
+                $_SESSION['user_data'] = array(
+                    "id" => $row['id'],
+                    "name" => $row['name'],
+                    "email" => $row['email']
+                );
+                header('Location:' . ROOT_URL . 'shares');
+            } else {
+                Messages::setMsg('Incorrect Login', 'error');
+            }
+        }
+        return;
+    }
+}
