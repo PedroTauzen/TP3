2b4c249e6bfe87e7c253f1e1e724a3bb454ee01f
Pedro Tauzen <pedrotauzen@gmail.com>
2023-12-16 16:48:58 +0000

    Finished Views

--- config.php
+++ config.php
@@ -6,6 +6,6 @@ const DB_USER = "root";
 const DB_PASS = "";
 const DB_NAME = "shareboard";
 
-// Define
-const ROOT_PATH = "/daw/TP3/";
+// Define the URL and PATH
+const ROOT_PATH = "C:/src/xampp/htdocs/daw/TP3/";
 const ROOT_URL = "http://localhost/daw/TP3/";
